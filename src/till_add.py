605f494d2b482a26effa5e88607285f67b755c63
sarah-larkin <larkinasarah@gmail.com>
2026-07-20 12:43:50 +0100

    refactor and alternative option explored

--- src/till_add.py
+++ src/till_add.py
@@ -4,35 +4,90 @@ arg: dict
 return str
 """
 
-def till_add(till): 
-    money = {
-        "1p": 0.01,
-        "2p": 0.02,
-        "5p": 0.05,
-        "10p": 0.1,
-        "20p": 0.2,
-        "50p": 0.5,
-        "£1": 1.0,
-        "£2": 2.0,
-        "£5": 5.0,
-        "£10": 10.0,
-        "£20": 20.0,
-        "£50": 50.0
-        }
+# ----------------
+# first attempt
+# ----------------
+
+# def till_add(till): 
+#     money = {
+#         "1p": 0.01,
+#         "2p": 0.02,
+#         "5p": 0.05,
+#         "10p": 0.1,
+#         "20p": 0.2,
+#         "50p": 0.5,
+#         "£1": 1.0,
+#         "£2": 2.0,
+#         "£5": 5.0,
+#         "£10": 10.0,
+#         "£20": 20.0,
+#         "£50": 50.0
+#         }
     
+#     total = 0 
+
+#     for key, value in till.items(): 
+#         total += money[key] * value
+#     print(total)
+#     return "£" + str(total)
+  
+# # below format is not necessary 
+#     # if total < 1: 
+#     #     total = int(total * 100 )
+#     #     return str(total) + "p" 
+    
+#     # if total > 1: 
+#     #     return "£" + str(total)
+
+
+#-------------
+# refactor
+# -----------
+
+# def till_add(till): 
+#     # best to avoid floats if possible for better accuracy
+#     money = {
+#         "1p": 1,
+#         "2p": 2,
+#         "5p": 5,
+#         "10p": 10,
+#         "20p": 20,
+#         "50p": 50,
+#         "£1": 100,
+#         "£2": 200,
+#         "£5": 500,
+#         "£10": 1000,
+#         "£20": 2000,
+#         "£50": 5000
+#         }
+
+#     # total = 0 
+
+#     # for key, value in till.items(): 
+#     #     total += money[key] * value
+#     # print(total)
+#     # return f"£{total / 100:.2f}"
+
+#     total = sum(money[key] * value for key, value in till.items())
+#     return f"£{total / 100:.2f}"
+
+
+#--------------
+# alternative
+#--------------
+
+def till_add(till): 
     total = 0 
 
-    for key, value in till.items(): 
-        total += money[key] * value
-    print(total)
+    for money, qty in till.items():
+        if money.endswith("p"): 
+            value = int(money[:-1])
+        else: 
+            value = int(money[1:]) * 100
 
-    if total < 1: 
-        total = int(total * 100 )
-        return str(total) + "p"
-    
-    if total > 1: 
-        return "£" + str(total)
+        total += value * qty 
 
+    return f"£{total / 100:.2f}"
 
-
-
+# no fixed lookup table required, extract direct from info given
+# no floats - convert to penny value then convert back to £
--- test/test_till_add.py
+++ test/test_till_add.py
@@ -4,22 +4,42 @@ def test_returns_string():
     result = till_add({ "1p": 1, "2p": 1 })
     assert isinstance(result, str)
 
+# def test_3p_addition(): 
+#     till = {"1p": 1,"2p": 1}
+#     result = till_add(till)
+#     assert result == "3p"
+
 def test_3p_addition(): 
     till = {"1p": 1,"2p": 1}
     result = till_add(till)
-    assert result == "3p"
+    assert result == "£0.03"
+
+# def test_38p_addition(): 
+#     till = { "1p": 1, "2p": 1, "5p": 1, "10p": 1, "20p": 1 }
+#     result = till_add(till)
+#     assert result == "38p"
 
 def test_38p_addition(): 
     till = { "1p": 1, "2p": 1, "5p": 1, "10p": 1, "20p": 1 }
     result = till_add(till)
-    assert result == "38p"
+    assert result == "£0.38"
 
 def test_over_one_pound_addition(): 
     till = { "5p": 1, "10p": 1, "20p": 1, "50p": 1, "£1": 1 }
     result = till_add(till)
     assert result == "£1.85"
 
+# def test_value_above_one(): 
+#     till = { "1p": 2, "2p": 3 }
+#     result = till_add(till)
+#     assert result == "8p"
+
 def test_value_above_one(): 
     till = { "1p": 2, "2p": 3 }
     result = till_add(till)
-    assert result == "8p"
+    assert result == "£0.08"
+
+def test_penny_value_divisible_by_10(): 
+    till = { "10p": 8 }
+    result = till_add(till)
+    assert result == "£0.80"
