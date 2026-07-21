1fdbbb0da262905ac83793974823df9b2bb5eb20
sarah-larkin <larkinasarah@gmail.com>
2026-07-21 11:05:28 +0100

    change calculator completed

--- src/change_calc.py
+++ src/change_calc.py
@@ -1,35 +1,49 @@
 
 def change_calculator(change_needed): 
-    """calculates the coins needed for change using the smallest amount of change necessary
+    """
+    Return the minimum number of UK coins required to make the given amount of change.
 
     Args:
         arg (int): total amount of change required in pence 
+
+    Returns:
+        dict[str, int] : coin values, quantities 
+
+    Raises: 
+        TypeError: If change_needed is not an int 
+        ValueError: If change_needed is negative 
     """
     coins_pennies = [50,20,10,5,2,1]
-    coins_pennies.sort(reverse=True) #to ensure always largest number first 
-
+    #coins_pennies.sort(reverse=True) #to ensure always largest number first - not necessary as fixed
 
     remaining_amount = change_needed
     result = {}
 
-    if change_needed < 100: 
-        for num in coins_pennies: 
-            if remaining_amount - num == 0:            #if only one coin required
-                result[str(num)] = 1
-                return result
-            if remaining_amount - num > 0 : 
-                remaining_amount -= num
-                result[str(num)] = 1 
-                # for num in coins_pennies: 
-                #     if remaining_amount - num == 0: 
-                #         result[str(num)] += 1
-                #     if remaining_amount - num > 0:
-                #         result[str(num)] += 1
-
-                #don't nest loop, add other conditions 
-    return result                      
-
+    if not isinstance(change_needed, int): 
+        raise TypeError("Enter valid number")
     
-    # return {}
+    if change_needed < 0: 
+        raise ValueError("Change cannot be negative")
+    
+    for coin in coins_pennies: 
+        while remaining_amount >= coin:  
+            if str (coin) not in result: 
+                result[str (coin)] = 1    #create key value pair in result dict
+            else: 
+                result[str (coin)] += 1   #increase the counter if already exists
+            remaining_amount -= coin
+    return result      
 
-change_calculator(3)
+            
+"""  
+Use the while loop to keep subtracting the same num repeatedly before moving 
+on with the loop
+
+>= coin (no longer looking to get to 0) 
+If remaining_amount bigger or == coin then keep taking from that coin in the coins_pennies list
+
+Use IF statements to validate inputs and raise the corresponding Exceptions 
+(only use try/except if calling code that could fail)
+
+check out .get()
+"""
--- test/test_change_calc.py
+++ test/test_change_calc.py
@@ -1,38 +1,75 @@
 from src.change_calc import change_calculator
+import pytest
 
-def test_change_calc_returns_dict(): 
-    input = 1
-    result = change_calculator(input)
-    assert isinstance(result, dict)
+class TestStructure:
+    def test_change_calc_returns_dict(self): 
+        input = 1
+        result = change_calculator(input)
+        assert isinstance(result, dict)
+    def test_zero_change(self): 
+        input = 0
+        result = change_calculator(input)
+        assert result == {}
 
-class TestOneCoinEach():
+
+class TestOneCoinEach:
     def test_result_for_one_penny(self): 
         input = 1
         result = change_calculator(input)
         assert result == {'1':1}
-
     def test_result_for_2p(self): 
         input = 2
         result = change_calculator(input)
         assert result == {'2':1}
-
     def test_result_for_3p(self): 
         input = 3
         result = change_calculator(input)
         assert result == {'2':1, '1':1}
-
     def test_result_for_7p(self): 
         input = 7
         result = change_calculator(input)
         assert result == {'5':1, '2':1}
-    
     def test_result_for_13p(self): 
         input = 13
         result = change_calculator(input)
         assert result == {'10':1, '2':1, '1':1}
 
-class TestMultipleCoins():
+class TestMultipleCoins:
     def test_result_for_4p(self): 
         input = 4
         result = change_calculator(input)
         assert result == {'2':2}
+    def test_result_for_40p(self): 
+        input = 40
+        result = change_calculator(input)
+        assert result == {'20':2}
+    def test_result_for_44p(self): 
+        input = 44
+        result = change_calculator(input)
+        assert result == {'20':2, '2':2}
+    def test_result_for_60p(self): 
+        input = 60
+        result = change_calculator(input)
+        assert result == {'50':1, '10': 1}
+    def test_result_for_99p(self): 
+        input = 99
+        result = change_calculator(input)
+        assert result == {'50': 1,'20':2, '5':1, '2':2}
+    def test_result_for_150p(self): 
+        input = 150
+        result = change_calculator(input)
+        assert result == {'50': 3}
+
+class TestExceptions: 
+    def test_change_is_wrong_type(self): 
+        with pytest.raises(TypeError):
+            change_calculator("10")
+
+    def test_no_change(self): 
+        with pytest.raises(TypeError):
+            change_calculator()
+    #python raises this, not handled in code 
+    
+    def test_neg_change(self):
+        with pytest.raises(ValueError):
+            change_calculator(-5)
