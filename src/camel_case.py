104e8e1cf5b2b5fc970c8cce1f30b1f4fcc130cc
sarah-larkin <larkinasarah@gmail.com>
2026-07-22 10:49:57 +0100

    refactored camel_case func

--- src/camel_case.py
+++ src/camel_case.py
@@ -1,4 +1,46 @@
 
+# def to_camel_case(phrase: str, case: bool):
+#     """Convert sentence to upper or lower camel case 
+
+#     Args: 
+#         arg (str) : sentence to convert to upper or lower camel case 
+#         arg (bool): True for UpperCamelCase, False for LowerCamelCase 
+    
+#     Returns: 
+#         str : sentence in upper or lower camel case 
+
+#     Raises: 
+
+
+#     """
+    
+#     if not isinstance(phrase, str):
+#         raise TypeError()
+    
+#     if not isinstance(case, bool) :
+#         raise TypeError()
+    
+#     if phrase: 
+#         if case == True: 
+#             split_phrase = phrase.split()
+#             capitalized_phrase = [word.capitalize() for word in split_phrase]
+#             upper_camel_case = "".join(capitalized_phrase)
+#             return upper_camel_case
+#         if case == False: 
+#             split_phrase = phrase.split()
+#             first_word = split_phrase[0].lower()  # str
+#             remaining_phrase = split_phrase[1:]   # list
+#             capitalized_phrase = [word.capitalize() for word in remaining_phrase]
+#             capitalized_phrase_joined = "".join(capitalized_phrase) # str
+#             combined_phrase = first_word + capitalized_phrase_joined 
+#             return combined_phrase
+#     else: 
+#         return "phrase needed"
+    
+#------------
+# refactor
+#------------
+
 def to_camel_case(phrase: str, case: bool):
     """Convert sentence to upper or lower camel case 
 
@@ -10,31 +52,31 @@ def to_camel_case(phrase: str, case: bool):
         str : sentence in upper or lower camel case 
 
     Raises: 
-
+        TypeError: If args are not str & bool
+        ValueError: If str in empty
 
     """
     
     if not isinstance(phrase, str):
-        raise TypeError()
+        raise TypeError("phrase must be a string")
     
     if not isinstance(case, bool) :
-        raise TypeError()
+        raise TypeError("case must be a boolean")
     
-    if phrase: 
-        if case == True: 
-            split_phrase = phrase.split()
-            capitalized_phrase = [word.capitalize() for word in split_phrase]
-            upper_camel_case = "".join(capitalized_phrase)
-            return upper_camel_case
-        if case == False: 
-            split_phrase = phrase.split()
-            first_word = split_phrase[0].lower()  # str
-            remaining_phrase = split_phrase[1:]   # list
-            capitalized_phrase = [word.capitalize() for word in remaining_phrase]
-            capitalized_phrase_joined = "".join(capitalized_phrase) # str
-            combined_phrase = first_word + capitalized_phrase_joined 
-            return combined_phrase
-    else: 
-        return "phrase needed"
+    if phrase == "": 
+        raise ValueError("phrase cannot be empty")
+    
+    split_phrase = phrase.split()  #avoids repeats
+       
+    if case: 
+        return "".join(word.capitalize() for word in split_phrase)
+    if not case: 
+        first_word = split_phrase[0].lower()  # str
+        remaining_phrase = split_phrase[1:]   # list
+        capitalized_phrase = [word.capitalize() for word in remaining_phrase]
+        capitalized_phrase_joined = "".join(capitalized_phrase) # str
+        combined_phrase = first_word + capitalized_phrase_joined 
+        return combined_phrase
+
     
     
--- test/test_camel_case.py
+++ test/test_camel_case.py
@@ -4,8 +4,8 @@ import pytest
 class TestForm: 
     def test_empty_str(self): 
         test_phrase = ""
-        result = to_camel_case(test_phrase, True)
-        assert result == "phrase needed" 
+        with pytest.raises(ValueError): 
+            to_camel_case(test_phrase, True)
     
     def test_wrong_type_for_str_value(self): 
         test_phrase = 123
@@ -23,7 +23,12 @@ class TestForm:
         assert isinstance(result, str)
 
 class TestOutput: 
-    def test_returns_upper_camel_case_when_true(self): 
+    def test_returns_upper_camel_case_for_single_word_when_true(self): 
+        test_phrase = "hello"
+        result = to_camel_case(test_phrase, True)
+        assert result == "Hello"
+    
+    def test_returns_upper_camel_case_for_phrase_when_true(self): 
         test_phrase = "this sentence"
         result = to_camel_case(test_phrase, True)
         assert result == "ThisSentence"
@@ -33,7 +38,22 @@ class TestOutput:
         result = to_camel_case(test_phrase, True)
         assert result == "ThisBiggerStrangeSentence"
 
-    def test_returns_lower_camel_case_when_false(self): 
+    def test_returns_upper_camel_case_When_additional_white_space_gaps(self): 
+        test_phrase = "This    Bigger   strange        Sentence"
+        result = to_camel_case(test_phrase, True)
+        assert result == "ThisBiggerStrangeSentence"
+
+    def test_returns_upper_camel_case_with_leading_trailing_white_space(self): 
+        test_phrase = "      This bigger Strange sentence          "
+        result = to_camel_case(test_phrase, True)
+        assert result == "ThisBiggerStrangeSentence"
+    
+    def test_returns_upper_camel_case_for_single_word_when_false(self): 
+        test_phrase = "hello"
+        result = to_camel_case(test_phrase, False)
+        assert result == "hello"
+
+    def test_returns_lower_camel_case_for_phrase_when_false(self): 
         test_phrase = "this sentence"
         result = to_camel_case(test_phrase, False)
         assert result == "thisSentence"
@@ -42,3 +62,14 @@ class TestOutput:
         test_phrase = "This Bigger strange Sentence"
         result = to_camel_case(test_phrase, False)
         assert result == "thisBiggerStrangeSentence"
+
+    def test_return_camel_case_when_already_camel_case(self): 
+        test_phrase_1 = "ThisSentence"
+        result_1 = to_camel_case(test_phrase_1, False)
+
+        test_phrase_2 = "thisSentence"
+        result_2 = to_camel_case(test_phrase_2, True)
+
+        assert result_1 == "thissentence"
+        assert result_2 == "Thissentence"
+        #func requires white space so will not work if already camel case 
