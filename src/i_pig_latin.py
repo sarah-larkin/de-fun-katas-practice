c608ae5086bf0e368fa0bb5947badc0963533a6b
sarah-larkin <larkinasarah@gmail.com>
2026-07-30 13:16:21 +0100

    solution completed that passes tests

--- src/i_pig_latin.py
+++ src/i_pig_latin.py
@@ -1,3 +1,5 @@
+import pytest 
+
 def pig_latin(phrase): 
     """translate english str to pig latin.
     The first consonant (or consecutive consonants) of each word is moved to the end and 'ay' is added.
@@ -6,6 +8,7 @@ def pig_latin(phrase):
     Args:
         phrase (str): english phrase 
     """
+    #edge cases
     if not phrase: 
         raise ValueError("full phrase needed ")
 
@@ -13,28 +16,46 @@ def pig_latin(phrase):
         raise TypeError("phrase must be a string")
     
     
+    #code: 
 
     vowels = ['a', 'e', 'i', 'o', 'u']
-
     split_phrase = phrase.split()
-
     output = []
 
     for word in split_phrase: 
-        if word[0] not in vowels and word[1] not in vowels: 
-            pig_word = word[2:] + word[:2] + 'ay'
-            output.append(pig_word)
-        elif word[0] not in vowels and word[1] in vowels: 
-            pig_word = word[1:] + word[0] + 'ay'
-            output.append(pig_word)
-        else: 
+        prefix = ""
+        if word[0] in vowels: 
             pig_word = word + 'way'
             output.append(pig_word)
-
+        else: 
+            for letter in word: 
+                if letter not in vowels: 
+                    prefix += letter
+                else: 
+                    break
+                pig_word = word[len(prefix):] + prefix + 'ay'
+                print(pig_word)
+            output.append(pig_word)
 
     result = " ".join(output) 
-
     return result
+
+
+    #first attempt
+       
+    # for word in split_phrase: 
+    #     if word[0] not in vowels and word[1] not in vowels: 
+    #         pig_word = word[2:] + word[:2] + 'ay'
+    #         output.append(pig_word)
+    #     elif word[0] not in vowels and word[1] in vowels: 
+    #         pig_word = word[1:] + word[0] + 'ay'
+    #         output.append(pig_word)
+    #     else: 
+    #         pig_word = word + 'way'
+    #         output.append(pig_word)
+
+
+   
 
 
 
@@ -43,3 +64,5 @@ def pig_latin(phrase):
 pig_latin("northcoders")
 pig_latin("sheffield")
 pig_latin("algorithm")
+pig_latin("three")
+pig_latin("keep on coding")
